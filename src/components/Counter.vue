10c07af814a8f39bdef23289e5a8ff5a3e2d956d
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-03-04 11:50:39 +0100

    test

---
+++ src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <form>
+    <div>
+      <h1>Buy Token- TEST</h1>
+     
+      <div>
+        <button type="button" class="signupbtn" v-on:click="test">Sign Up</button>
+        <h1>{{message}}</h1>
+      </div>
+    </div>
+  </form>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        message: 1
+      }
+    },
+    methods: {
+      test(){
+        this.message = this.message + 1;
+        console.log("Mohit");
+      }
+    },
+  }
+</script>
+
+<style  scoped>
+
+</style>
